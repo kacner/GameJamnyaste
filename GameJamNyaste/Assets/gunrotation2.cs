3a1a33f08e807f55c7a54aca15ae1fc075184616
kacner <Einar.lundqvist@gmail.com>
2024-01-17 13:16:51 +0100

    camera

--- GameJamNyaste/Assets/gunrotation2.cs
+++ GameJamNyaste/Assets/gunrotation2.cs
@@ -16,6 +16,19 @@ public class gunrotation2 : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
 
+        if (rotationZ < -90 || rotationZ > 90)
+        {
+            if (myPlayer.transform.eulerAngles.y == 0)
+            {
+                transform.localRotation = Quaternion.Euler(180, 0, -rotationZ);
+            }
+            else if (myPlayer.transform.eulerAngles.y == 180)
+            {
+                transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);
+            }
+
+        }
+
 
     }
 
