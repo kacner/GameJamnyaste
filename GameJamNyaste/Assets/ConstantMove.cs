fbdaeb13e89f1fa4476101425c9e777535067f2f
kacner <Einar.lundqvist@gmail.com>
2024-01-18 21:23:52 +0100

    marquesBrowley

--- GameJamNyaste/Assets/ConstantMove.cs
+++ GameJamNyaste/Assets/ConstantMove.cs
@@ -6,6 +6,7 @@ public class ConstantMove : MonoBehaviour
 {
     public Vector2 movementDirection = new Vector2(1f, 0f); // Change this vector to specify the movement direction
     public float movementSpeed = 5f; // Change this value to set the movement speed
+    public GameObject targetObject; // Drag and drop the target GameObject in the inspector
     private bool touchingPlayer = false;
 
     private void Start()
@@ -18,7 +19,8 @@ public class ConstantMove : MonoBehaviour
         if (!touchingPlayer)
         {
             // Calculate the new position based on the direction and speed
-            Vector3 newPosition = transform.position + new Vector3(movementDirection.x, movementDirection.y, 0f) * movementSpeed * Time.deltaTime;
+            float targetY = targetObject.transform.position.y;
+            Vector3 newPosition = new Vector3(transform.position.x, targetY, 0f) + new Vector3(movementDirection.x, movementDirection.y, 0f) * movementSpeed * Time.deltaTime;
 
             // Update the position of the GameObject
             transform.position = newPosition;
@@ -31,6 +33,10 @@ public class ConstantMove : MonoBehaviour
         {
             touchingPlayer = true;
         }
+        if (other.CompareTag("FakePlayer"))
+        {
+            movementSpeed = 0;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,6 +44,10 @@ public class ConstantMove : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             touchingPlayer = true;
+        }
+        if (collision.gameObject.CompareTag("FakePlayer"))
+        {
+            movementSpeed = 0;
         }
     }
 
